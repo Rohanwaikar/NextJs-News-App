44c6f8a7ed58cdf94fd219c33790b8283b20ea7a
Rohan Waikar <rohanwaikar@Rohans-MacBook-Air.local>
2025-07-29 20:42:33 +0530

    updated next.config.js file

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // ✅ for SSR deployment (not 'export')
+};
+
+export default nextConfig;
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Required if you're using <Image /> without server support
-  },
-};
-
-export default nextConfig;
